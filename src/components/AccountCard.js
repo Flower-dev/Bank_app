f073ed67c4263e451ac93986ac25954599e54916
Flower Dev <67862441+Flower-dev@users.noreply.github.com>
2022-03-10 21:21:37 +0100

    user view

---
+++ src/components/AccountCard.js
@@ -0,0 +1,17 @@
+// custom
+import '../custom/accountCard.scss';
+
+export default function AccountCard({title, amount, description}) {
+    return (
+        <div className="account">
+            <div className="account-content-wrapper">
+                <h3 className="account-title">{title}</h3>
+                <p className="account-amount">{amount}</p>
+                <p className="account-amount-description">{description}</p>
+            </div>
+            <div className="account-content-wrapper cta">
+                <button className="transaction-button">View transactions</button>
+            </div>
+      </div>
+    )
+}
--- src/pages/User.js
+++ src/pages/User.js
@@ -1,3 +1,49 @@
+// components
+import AccountCard from '../components/AccountCard';
+// custom
+import '../custom/pages/user.scss';
+
+
 export default function User() {
-    return (<h1>Page user</h1>)
+    // mock data
+    // TO DO -> call API 
+    const DATA = [
+        {
+            id: 1,
+            title: 'Argent Bank Checking (x8349)',
+            amount: '$2,082.79',
+            description: 'Available Balance',
+        },
+        {
+            id: 2,
+            title: 'Argent Bank Savings (x6712)',
+            amount: '$10,928.42',
+            description: 'Available Balance',
+        },
+        {
+            id: 3,
+            title: 'Argent Bank Credit Card (x8349)',
+            amount: '$184.30',
+            description: 'Current Balance',
+        }
+    ]
+
+    return (
+        <div className="main bg-dark">
+            <div className="header">
+                <h1>Welcome back<br/>Tony Jarvis!</h1>
+                <button className="edit-button">Edit Name</button>
+            </div>
+            {DATA.map((item) => {
+                return (
+                    <AccountCard
+                        key={item.id}
+                        title={item.title}
+                        amount={item.amount}
+                        description={item.description}
+                    />
+                )
+            })}
+        </div>
+    )
 }
