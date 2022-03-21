a8f3565c903f0cfd74e456f298d8e36dedc5f207
Flower Dev <67862441+Flower-dev@users.noreply.github.com>
2022-03-21 12:19:34 +0100

    profile info redux

--- src/pages/User.js
+++ src/pages/User.js
@@ -1,3 +1,9 @@
+import { useEffect, useState } from 'react';
+// redux
+import { useDispatch, useSelector } from 'react-redux';
+import { setInfos } from '../redux/slice'
+// hooks
+import  useApi from '../hooks/useApi'
 // components
 import AccountCard from '../components/AccountCard';
 // custom
@@ -5,8 +11,65 @@ import '../custom/pages/user.scss';
 
 
 export default function User() {
-    // mock data
-    // TO DO -> call API 
+    const token = useSelector((state) => state.user.token);
+    const firstName = useSelector((state) => state.user.infos.firstName);
+    const lastName = useSelector((state) => state.user.infos.lastName);
+    const [firstNameForm, setFirstNameForm] = useState('');
+    const [lastNameForm, setLastNameForm] = useState('');
+    const [displayForm, setDisplayForm] = useState(false);
+    const dispatch = useDispatch();
+
+    const onFirstNameChange = (e) => {
+        setFirstNameForm(e.target.value);
+    }
+
+    const onLastNameChange = (e) => {
+        setLastNameForm(e.target.value);
+    }
+
+    useEffect(() => {
+        try {
+            const getInfos = async () => {
+                const userData = await useApi.getInfos(token);
+                const { email, firstName, lastName, id } = userData;
+                const infos = { email, firstName, lastName, id };
+                dispatch(setInfos(infos));
+                setFirstNameForm(firstName);
+                setLastNameForm(lastName);
+            }
+            getInfos();
+        } catch (e) {
+            console.log(e);
+        }
+    }, [token]);
+
+    const onEdit = () => {
+        if (!displayForm) {
+            setDisplayForm(true);
+        }
+    }
+
+    const onSave = async (e) => {
+        try {
+            e.preventDefault();
+            const updateUserData = await useApi.updateInfos(firstNameForm, lastNameForm, token);
+            const { firstName, lastName } = updateUserData;
+            const infosUpdate = { firstName, lastName };
+            dispatch(setInfos(infosUpdate));
+            if (displayForm) {
+                setDisplayForm(false);
+            }
+        } catch (e) {
+            console.log(e);
+        }
+    }
+
+    const onCancel = () => {
+        if (displayForm) {
+            setDisplayForm(false);
+        }
+    }
+ 
     const DATA = [
         {
             id: 1,
@@ -31,8 +94,25 @@ export default function User() {
     return (
         <div className="main bg-dark">
             <div className="header">
-                <h1>Welcome back<br/>Tony Jarvis!</h1>
-                <button className="edit-button">Edit Name</button>
+                <h1>Welcome back</h1>
+                { displayForm ?
+                    (
+                        <form>
+                            <input type="text" onChange={onFirstNameChange} value={firstNameForm} placeholder={firstNameForm} />
+                            <input type="text" onChange={onLastNameChange} value={lastNameForm} placeholder={lastNameForm} />
+                            <div>
+                                <button onClick={onSave} className="edit-button">Save</button>
+                                <button onClick={onCancel} className="edit-button">Cancel</button>
+                            </div>
+                        </form>
+                    ) :
+                    (
+                        <div>
+                            <h1>{firstName} {lastName}!</h1>
+                            <button onClick={onEdit} className="edit-button">Edit Name</button>
+                        </div>
+                    )
+                }
             </div>
             {DATA.map((item) => {
                 return (
