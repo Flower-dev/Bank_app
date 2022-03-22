535065def72446c293b6dc8bad42ca5411de6a7d
Flower Dev <67862441+Flower-dev@users.noreply.github.com>
2022-03-22 12:05:38 +0100

    fix() custom loginForm & login page

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,18 +1,27 @@
-import React, { useState } from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faUserCircle } from '@fortawesome/free-solid-svg-icons';
+import { useState } from 'react';
+// routes
 import { useHistory } from 'react-router-dom';
 // redux
 import { useDispatch } from 'react-redux';
 import { setToken } from '../redux/slices/user';
 // hooks
 import useApi from '../hooks/useApi';
+// icons (fontawesome lib)
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faUserCircle } from '@fortawesome/free-solid-svg-icons';
+// custom
+import '../custom/loginForm.scss'
 
+// ----------------------------------------------------------
 export default function LoginForm() {
+    // redux
+    const dispatch = useDispatch();
+    // routes
+    const history = useHistory();
+    // state
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
-    const dispatch = useDispatch();
-    const history = useHistory();
+    
 
     const onEmailChange = (e) => {
         setEmail(e.target.value);
@@ -36,31 +45,29 @@ export default function LoginForm() {
     }
 
     return (
-        <div>
-            <main className='main bg-dark'>
-                <section className='sign-in-content'>
-                    <FontAwesomeIcon icon={faUserCircle} className='sign-in-icon'/>
-                    <h1>Sign In</h1>
-                    <form>
-                        <div className='input-wrapper'>
-                            <label htmlFor='username'>Username</label>
-                            <input type='text' id='username' value={email} onChange={onEmailChange}/>
-                        </div>
+        <main className='main bg-dark'>
+            <section className='sign-in-content'>
+                <FontAwesomeIcon icon={faUserCircle} className='sign-in-icon'/>
+                <h1>Sign In</h1>
+                <form>
+                    <div className='input-wrapper'>
+                        <label htmlFor='username'>Username</label>
+                        <input type='text' id='username' value={email} onChange={onEmailChange}/>
+                    </div>
 
-                        <div className='input-wrapper'>
-                            <label htmlFor='password'>Password</label>
-                            <input type='password' id='password' value={password} onChange={onPasswordChange}/>
-                        </div>
+                    <div className='input-wrapper'>
+                        <label htmlFor='password'>Password</label>
+                        <input type='password' id='password' value={password} onChange={onPasswordChange}/>
+                    </div>
 
-                        <div className='input-remember'>
-                            <input type='checkbox' id='remember-me'/>
-                            <label htmlFor='remember-me'>Remember me</label>
-                        </div>
+                    <div className='input-remember'>
+                        <input type='checkbox' id='remember-me'/>
+                        <label htmlFor='remember-me'>Remember me</label>
+                    </div>
 
-                        <button onClick={onSubmit} className='sign-in-button'>Sign In</button>
-                    </form>
-                </section>
-            </main>
-        </div>
+                    <button onClick={onSubmit} className='sign-in-button'>Sign In</button>
+                </form>
+            </section>
+        </main>
     )
 }
